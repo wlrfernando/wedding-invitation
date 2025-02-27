b6bbb29f97a875a39dbfbb4b7fd9fd1a2b56950c
wlrfernando <wlrfernando@outlook.com>
2025-02-27 22:04:13 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import SVG2 from "./assets/svgs/svg2"
+
+function App() {
+
+  return (
+    <div className="container max-w-2xl max-h-screen mx-auto">
+      <div className="border">
+        <SVG2 />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
